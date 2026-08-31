b9edc0ee0df2523afcbb3fe70d16cf6fcd647aea
diyorbek73org <diyorbek770110@gmail.com>
2026-08-31 17:29:53 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = '8786245269:AAHRpmYTipc0mXWL5FWBydwuSXagj_6XV3w'
+ADMIN_ID = 8821384634  # Telegram ID raqamingiz
+
+SKIP_PENDING = False
+PLAYERS_COUNT_TO_START = 4
+PLAYERS_COUNT_LIMIT = 10
+REQUEST_OVERDUE_TIME = 10 * 60
+WORD_BASE = 'words.txt'
+DELETE_FROM_EVERYONE = False
+
+SET_WEBHOOK = False  # Polling rejimida barqaror ishlashi uchun False qilindi
+
+import logging
+LOGGER_LEVEL = logging.INFO
